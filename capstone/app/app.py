e8360fc582f79f9b858c5771bbd3b23cf6ff6c84
Tahj Gordon <tahjalex1@gmail.com>
2025-06-16 20:08:12 -0400

    users can now generate reports

--- capstone/app/app.py
+++ capstone/app/app.py
@@ -3,12 +3,15 @@ st.set_page_config(page_title="CareerPilot", layout="wide")
 
 from resume_optimizer import run_resume_optimizer
 from dashboard import run_dashboard
+from reports import run_reports
 
-
-tab1, tab2 = st.tabs(["📝 Resume Optimizer", "📊 Job Tracker"])
+tab1, tab2, tab3 = st.tabs(["📝 Resume Optimizer", "👣 Job Tracker","📄 Generate Reports"])
 
 with tab1:
     run_resume_optimizer()
 
 with tab2:
     run_dashboard()
+
+with tab3:
+    run_reports()

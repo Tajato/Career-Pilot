483a2d5b706796ec759ce5e0b9e67eaaa4fa5cb5
Tahj Gordon <tahjalex1@gmail.com>
2025-06-16 23:43:57 -0400

    prep for render deployment

--- capstone/app/database.py
+++ capstone/app/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-# SQLite database URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./job_tracker.db"
+# postgre database URL
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 
 engine = create_engine(
